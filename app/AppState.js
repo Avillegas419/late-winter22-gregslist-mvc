2ec97b43d04d07ef427d8dbb0fb0b024ccf79d1e
Ariel Sanchez <Arielv419@gmail.com>
2022-02-16 14:01:46 -0700

    change cars to car

--- app/AppState.js
+++ app/AppState.js
@@ -4,20 +4,20 @@ import { isValidProp } from "./Utils/isValidProp.js"
 
 const testCar = new Car(
   {
-    make:'GMC', 
-    model:'Sierra', 
-    year:2018, 
-    price:5600, 
+    make: 'GMC',
+    model: 'Sierra',
+    year: 2018,
+    price: 5600,
     description: 'its cool.',
-    color:'#FFFFFF', 
-    imgUrl:'https://thiscatdoesnotexist.com'
+    color: '#FFFFFF',
+    imgUrl: 'https://www.gmc.com/content/dam/gmc/na/us/english/index/shared-assets/jellybeans/2022/sierra/sierra-1500/01-images/2022-sierra-1500-pro-summit-white.png?imwidth=960'
   })
 
 class AppState extends EventEmitter {
-  
+
   // NOTE just adds intellisense to our cars array that lets our code know its an array of cars, not other things 
   /** @type {import('./Models/Car').Car[]} */
-  cars =[testCar]
+  cars = [testCar]
 }
 
 
